ae437e120b71c4da967e6a453cc7a0d83cc7566e
firmanriawan9 <31408277+firmanriawan9@users.noreply.github.com>
2017-10-20 19:59:55 +0700

    Add files via upload

---
+++ Iklan Elsword.js
@@ -0,0 +1,44 @@
+<script type="text/javascript">
+var arldgzn_id = 4;
+var arldgzn_now = new Date()
+var detik = arldgzn_now.getSeconds()
+var arldgzn_place = detik % arldgzn_id;
+arldgzn_place += 1;
+if (arldgzn_place == 1) {
+  url = "https://goo.gl/ef6dev";
+  alt = "";
+  title = "";
+  banner = "https://2.bp.blogspot.com/-OLvT54mOndI/Wenvqr6E1fI/AAAAAAAAAmQ/CoguPEkidjA8BHEMujcg6-E_n0mVuzgqgCEwYBhgL/s1600/4635467333049326180.jpg";
+  width = "728";
+  height = "90";
+}
+if (arldgzn_place == 2) {
+  url = "https://goo.gl/ef6dev";
+  alt = "";
+  title = "";
+  banner = "https://1.bp.blogspot.com/-KIm6i6JsRlw/WenvtC_e-KI/AAAAAAAAAmc/FAn5EHyQVGQvOmU1OR0EarcxhSm4OMtigCEwYBhgL/s1600/806962882674787195.jpg";
+  width = "728";
+  height = "90";
+}
+if (arldgzn_place == 3) {
+  url = "https://goo.gl/ef6dev";
+  alt = "";
+  title = "";
+  banner = "https://3.bp.blogspot.com/-5w4zJAK0hd0/WeiN3NjE9RI/AAAAAAAAAk8/wjpQ5mo9KPYJqI03xEvnp5FW4K0wyjc2wCPcBGAYYCw/s1600/5430694245428112725.jpg";
+  width = "728";
+  height = "90";
+}
+
+if (arldgzn_place == 4) {
+  url = "https://goo.gl/ef6dev";
+  alt = "";
+  title = "";
+  banner = "https://2.bp.blogspot.com/-XLEI9MY5maI/Wenx0RzlN0I/AAAAAAAAAmk/0t2FFHEguiU7P1sbryL3dsmQVO1dTFjFQCLcBGAs/s1600/1.jpg";
+  width = "728";
+  height = "90";
+}
+document.write('<a href=\"' + url + '\" target=\"_blank\" rel=\"nofollow\">');
+document.write('<img src=\"' + banner + '\" width=')
+document.write(width + ' height=' + height + ' ');
+document.write('title=\"' + title + '\" alt=\"' + alt + '\"></a>');
+</script>
